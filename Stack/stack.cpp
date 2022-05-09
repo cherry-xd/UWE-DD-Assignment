7dc3b5dc887e9b26ca642b9295b2ee77c6e1b8a8
Luke Emrys Walker <81542899+cherry-xd@users.noreply.github.com>
2022-05-09 19:30:07 +0100

    Fixed a major bug causing pop function to break

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -4,45 +4,46 @@
 using namespace std;
 
 STACK::STACK() {
-    top=-1;
+    top = -1;
 }
 
 int STACK::isEmpty() {
-    if(top==-1)
+    if (top == -1)
         return 1;
     else
         return 0;
 }
 
 int STACK::isFull() {
-    if(top==(SIZE-1))
+    if (top == (SIZE - 1))
         return 1;
     else
         return 0;
 }
 
 int STACK::push(int i) {
-    if(isFull()) {
+    if (isFull()) {
         cout << "STACK FULL ERROR -30424\n";
         // This is so we know if the stack has reached its limit
         return -30424;
     }
     ++top;
-    num[top]=i;
+    num[top] = i;
     return i;
 }
 
 int STACK::pop() {
     int temp;
-    if(isEmpty())
+    if (isEmpty()) {
         cout << "STACK EMPTY ERROR -30425\n";
         // This error is so we know if the stack is empty
         return -30425;
-    temp=num[top];
+    }
+    temp = num[top];
     --top;
     return temp;
 }
 
 int STACK::size() {
-    return top+1;
+    return top + 1;
 }
